17f21652c95bb453f509758f901d4b3b70ed5b41
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-12 23:40:03 +0530

    updated content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponents} from "./home/home.components";
+import {CustomerComponent} from "./customer/customer.component";
+import {OrdersComponent} from "./orders/orders.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponents},
+  {path:'customer',component:CustomerComponent},
+  {path:'orders',component:OrdersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
